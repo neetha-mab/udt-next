a7d6c1f99f99c7af925eb23e0febf2d3aba96dcc
neetha <neetha@mabventures.com>
2022-09-02 14:35:18 +0530

    fix: imae issue

--- components/pageComponents/home/WhyWeAreHere.js
+++ components/pageComponents/home/WhyWeAreHere.js
@@ -17,7 +17,8 @@ const WhyWeAreHere = ({data}) => {
   return (
     <section className={styles.why_we_are_here}>
         <div className={styles.image_wrap} data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-            <div className={styles.desktop}><Image src={imageUrl} alt={alt} width={720} height={1064} layout={'responsive'} /></div>
+            <div className={styles.desktop}><Image src={imageUrl} alt={alt} objectFit={'cover'} layout={'fill'} /></div>
+            {/* <div className={styles.desktop}><Image src={imageUrl} alt={alt} width={720} height={1064} layout={'responsive'} /></div> */}
             <div  className={styles.mobile}><Image src={imageUrl2} alt={alt} width={390} height={240} layout={'responsive'} /></div>
         </div>
         <div className={styles.text_wrap} data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
